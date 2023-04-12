c46702b2bbae76f2a66bfefda51319775dc65f61
Jashan Dhanda <jashandhanda@Jashans-MacBook-Pro.local>
2023-04-11 19:53:22 -0700

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     
       res.sendFile(__dirname + "/Register.html");
   });
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 6000, () => {
     console.log('Your app is listening on port ' + listener.address().port)
   })
 
